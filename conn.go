4afc9a8c416d58cb8743a5823b964fc6112cda78
Timo Beckers <timo@incline.eu>
2018-08-21 20:07:50 +0200

    conntrack - Conn.Listen(): concurrent Event decoder

--- conn.go
+++ conn.go
@@ -1,7 +1,10 @@
 package conntrack
 
 import (
+	"fmt"
+
 	"github.com/mdlayher/netlink"
+	"github.com/pkg/errors"
 	"github.com/ti-mo/netfilter"
 )
 
@@ -24,9 +27,79 @@ func Open() (*Conn, error) {
 
 // Close closes a Conn.
 func (c *Conn) Close() error {
-
-	// Call through to the Netfilter Close().
 	return c.conn.Close()
+}
+
+// Listen joins the Netfilter connection to a multicast group and starts a given
+// amount of Flow decoders from the Conn to the Flow channel. Returns an error channel
+// the workers will return any errors on. Any errors during Flow decoding are fatal.
+//
+// The Conn will be marked as having listeners active, which will prevent Listen from being
+// called again. For listening on other groups, open another socket.
+func (c *Conn) Listen(evChan chan<- Event, numWorkers uint8, groups []netfilter.NetlinkGroup) (chan error, error) {
+
+	if numWorkers < 1 {
+		return nil, errors.Errorf(errWorkerCount, numWorkers)
+	}
+
+	// Prevent Listen() from being called twice on the same Conn.
+	if c.conn.IsMulticast() {
+		return nil, errConnHasListeners
+	}
+
+	err := c.conn.JoinGroups(groups)
+	if err != nil {
+		return nil, err
+	}
+
+	errChan := make(chan error)
+
+	// Start numWorkers amount of worker goroutines
+	for id := uint8(0); id < numWorkers; id++ {
+		go c.listenWorker(id, evChan, errChan)
+	}
+
+	return errChan, nil
+}
+
+// listenWorker is a worker function that decodes Netlink messages into Events.
+func (c *Conn) listenWorker(workerID uint8, evChan chan<- Event, errChan chan<- error) {
+
+	// Recover from panics in Receive when closing the Conn
+	defer func() {
+		if r := recover(); r != nil {
+			errChan <- fmt.Errorf(errRecover, "listenWorker", r)
+			return
+		}
+	}()
+
+	var err error
+	var recv []netlink.Message
+	var ev Event
+
+	for {
+		// Receive data from the Netlink socket
+		recv, err = c.conn.Receive()
+		if err != nil {
+			errChan <- errors.Wrap(err, fmt.Sprintf(errWorkerReceive, workerID))
+			return
+		}
+
+		// Receive() always returns a list of Netlink Messages, but multicast messages should never be multi-part
+		if len(recv) > 1 {
+			errChan <- errMultipartEvent
+			return
+		}
+
+		// Decode event and send on channel
+		err := ev.FromNetlinkMessage(recv[0])
+		if err != nil {
+			errChan <- err
+			return
+		}
+
+		evChan <- ev
+	}
 }
 
 // Dump gets all Conntrack connections from the kernel in the form of a list
--- errors.go
+++ errors.go
@@ -3,9 +3,15 @@ package conntrack
 import "errors"
 
 var (
-	errNotConntrack = errors.New("trying to decode a non-conntrack or conntrack-exp message")
+	errNotConntrack     = errors.New("trying to decode a non-conntrack or conntrack-exp message")
+	errConnHasListeners = errors.New("Conn has existing listeners, open another to listen on more groups")
+	errMultipartEvent   = errors.New("received multicast event with more than one Netlink message")
 )
 
 const (
 	errUnknownEventType = "unknown event type %d"
+	errWorkerCount      = "invalid worker count %s"
+	errMessageTypeRange = "message (event) type %x out of range for Netlink subscription"
+	errRecover          = "recovered from panic in function %s: %s"
+	errWorkerReceive    = "netlink.Receive error in listenWorker %d, exiting"
 )
